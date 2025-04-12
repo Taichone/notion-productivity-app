b3a0e98ffcdd8b535ad6203c6249b23eafecc40a
Taichone <taichone.dev@gmail.com>
2025-04-12 21:58:34 +0900

    feat: testDependency

--- NotionTimer/NotionTimerPackage/Sources/DataLayer/Util/DependencyClient.swift
+++ NotionTimer/NotionTimerPackage/Sources/DataLayer/Util/DependencyClient.swift
@@ -5,7 +5,16 @@
 //  Created by Taichi on 2025/04/02.
 //
 
-protocol DependencyClient: Sendable {
+public protocol DependencyClient: Sendable {
     static var liveValue: Self { get }
     static var testValue: Self { get }
+}
+
+public func testDependency<D: DependencyClient>(
+    of type: D.Type,
+    injection: (inout D) -> Void
+) -> D {
+    var dependencyClient = type.testValue
+    injection(&dependencyClient)
+    return dependencyClient
 }
